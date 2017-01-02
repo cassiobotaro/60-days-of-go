95a9b4bd70e40f3b2456b6b9dcfd224ac9813e31
Cássio Botaro <cassiobotaro@gmail.com>
2017-01-01 23:20:39 -0200

    Update hello_world.go

--- day01/hello_world.go
+++ day01/hello_world.go
@@ -6,6 +6,6 @@ import "fmt"
 
 // Every program should have a main function too, and will be the first executed function.
 func main() {
-	// Println is the function to print "Hello World!!" on the standard output and also break a line.
+	// Println print "Hello World!!" on the standard output and also break a line.
 	fmt.Println("Hello World!!")
 }
