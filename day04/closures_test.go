b0bd6c0932b4314729117ee666c90c8a4682bb65
cassiobotaro <cassiobotaro@gmail.com>
2017-01-04 21:46:52 -0200

    oops, fix tests

--- day04/closures_test.go
+++ day04/closures_test.go
@@ -6,10 +6,10 @@ import "testing"
 func TestSequence(t *testing.T) {
 	// initialize the sequence(closure is returned)
 	nextInt := intSequence()
-	for i := 0; i < 10; i++ {
+	for i := 1; i < 10; i++ {
 		// call nextInt n times
-		if nextInt() != i {
-			t.Errorf("expected %d but %d was obtained", sequence[i], value)
+		if n := nextInt(); n != i {
+			t.Errorf("expected %d but %d was obtained", i, v)
 		}
 	}
 }
