94810bd6c6f475a802b3fcb5067b39e04bc35789
cassiobotaro <cassiobotaro@gmail.com>
2017-02-03 23:20:29 -0200

    fix memory leak day 34

--- day34/writeinc.go
+++ day34/writeinc.go
@@ -4,7 +4,10 @@ package main
 #include "mystring.h"
 */
 import "C"
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
 	// CString wil return a *C.char
@@ -16,18 +19,30 @@ func main() {
 	fmt.Println("First index of \"b\" in \"test\"", C.firstIndexOf(original, C.char('b')))
 	fmt.Println("Last index of \"t\" in \"test\"", C.lastIndexOf(original, C.char('t')))
 	fmt.Println("Last index of \"b\" in \"test\"", C.lastIndexOf(original, C.char('b')))
-	fmt.Println(`"house" == "house"`, C.equals(C.CString("house"), C.CString("house")))
-	fmt.Println(`"house" == "houte"`, C.equals(C.CString("house"), C.CString("houte")))
+	house := C.CString("house")
+	houte := C.CString("houte")
+	fmt.Println(`"house" == "house"`, C.equals(house, house))
+	fmt.Println(`"house" == "houte"`, C.equals(house, houte))
 	C.toUpperCase(original)
 	fmt.Println("toUpperCase(\"test\")", C.GoString(original))
 	C.toLowerCase(original)
 	fmt.Println("toLowerCase(\"TEST\")", C.GoString(original))
-	fmt.Println("\"test\" == \"TEST\" ignoring case", C.equalsIgnoreCase(original, C.CString("TEST")))
-	fmt.Println("\"test\" == \"TAST\" ignoring case", C.equalsIgnoreCase(original, C.CString("TAST")))
+	testUpper := C.CString("TEST")
+	tastUpper := C.CString("TAST")
+	fmt.Println("\"test\" == \"TEST\" ignoring case", C.equalsIgnoreCase(original, testUpper))
+	fmt.Println("\"test\" == \"TAST\" ignoring case", C.equalsIgnoreCase(original, tastUpper))
 	sub := C.CString("")
 	C.substring(original, sub, 1, 4)
 	fmt.Println("teste (0, 4)", C.GoString(sub))
 	C.replace(original, C.char('e'), C.char('a'))
 	// Have to convert to a GoString
 	fmt.Println("Replace \"e\" to \"a\" in \"test\"", C.GoString(original))
+	// CString allocate memory using malloc
+	// you have to free memory manually
+	C.free(unsafe.Pointer(original))
+	C.free(unsafe.Pointer(house))
+	C.free(unsafe.Pointer(houte))
+	C.free(unsafe.Pointer(sub))
+	C.free(unsafe.Pointer(testUpper))
+	C.free(unsafe.Pointer(tastUpper))
 }
