daac5d6d6ae250fa2155ad23a83335e8a745256f
cassiobotaro <cassiobotaro@gmail.com>
2017-02-03 23:03:37 -0200

    more examples - day34

--- day34/writeinc.go
+++ day34/writeinc.go
@@ -9,10 +9,25 @@ import "fmt"
 func main() {
 	// CString wil return a *C.char
 	// it's equivalent to char* in C
-	original := C.CString("teste")
+	original := C.CString("test")
 	// length is included when i do #include in comments below
-	fmt.Println(C.length(C.CString("teste")))
+	fmt.Println("Length of \"test\"", C.length(original))
+	fmt.Println("First index of \"t\" in \"test\"", C.firstIndexOf(original, C.char('t')))
+	fmt.Println("First index of \"b\" in \"test\"", C.firstIndexOf(original, C.char('b')))
+	fmt.Println("Last index of \"t\" in \"test\"", C.lastIndexOf(original, C.char('t')))
+	fmt.Println("Last index of \"b\" in \"test\"", C.lastIndexOf(original, C.char('b')))
+	fmt.Println(`"house" == "house"`, C.equals(C.CString("house"), C.CString("house")))
+	fmt.Println(`"house" == "houte"`, C.equals(C.CString("house"), C.CString("houte")))
+	C.toUpperCase(original)
+	fmt.Println("toUpperCase(\"test\")", C.GoString(original))
+	C.toLowerCase(original)
+	fmt.Println("toLowerCase(\"TEST\")", C.GoString(original))
+	fmt.Println("\"test\" == \"TEST\" ignoring case", C.equalsIgnoreCase(original, C.CString("TEST")))
+	fmt.Println("\"test\" == \"TAST\" ignoring case", C.equalsIgnoreCase(original, C.CString("TAST")))
+	sub := C.CString("")
+	C.substring(original, sub, 1, 4)
+	fmt.Println("teste (0, 4)", C.GoString(sub))
 	C.replace(original, C.char('e'), C.char('a'))
 	// Have to convert to a GoString
-	fmt.Println(C.GoString(original))
+	fmt.Println("Replace \"e\" to \"a\" in \"test\"", C.GoString(original))
 }
